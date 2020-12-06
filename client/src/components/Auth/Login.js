d12abfdfd8800fad4de1a15f4e92f90bae77bdd3
Phonekham <30440409+Phonekham@users.noreply.github.com>
2020-12-06 10:46:08 +0700

    Add google Login Button & get idToken

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,9 +1,21 @@
 import React from "react";
 import { withStyles } from "@material-ui/core/styles";
+import { GoogleLogin } from "react-google-login";
 // import Typography from "@material-ui/core/Typography";
 
 const Login = ({ classes }) => {
-  return <div>Login</div>;
+  const onSuccess = (googleUser) => {
+    const idToken = googleUser.getAuthResponse().id_token;
+    console.log(idToken);
+  };
+
+  return (
+    <GoogleLogin
+      clientId="1072323787876-rh2ouk6thv0jg4mt2btbl5ljsjjnm705.apps.googleusercontent.com"
+      onSuccess={onSuccess}
+      isSignedIn={true}
+    />
+  );
 };
 
 const styles = {
@@ -12,8 +24,8 @@ const styles = {
     display: "flex",
     justifyContent: "center",
     flexDirection: "column",
-    alignItems: "center"
-  }
+    alignItems: "center",
+  },
 };
 
 export default withStyles(styles)(Login);
