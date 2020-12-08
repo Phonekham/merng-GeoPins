b42b157da06a2f422bbcf6e6c789af1cb8e36ffe
Phonekham <30440409+Phonekham@users.noreply.github.com>
2020-12-08 11:51:30 +0700

    adding draft update pin onclick

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { withStyles } from "@material-ui/core/styles";
 import ReactMapGL, { NavigationControl, Marker } from "react-map-gl";
 import PinIcon from "./PinIcon";
+import Context from "../context";
 // import Button from "@material-ui/core/Button";
 // import Typography from "@material-ui/core/Typography";
 // import DeleteIcon from "@material-ui/icons/DeleteTwoTone";
@@ -15,6 +16,7 @@ const INITIAL_VIEWPORT = {
 const Map = ({ classes }) => {
   const [viewport, setViewport] = useState(INITIAL_VIEWPORT);
   const [userPosition, setUserPosition] = useState(null);
+  const { state, dispatch } = useContext(Context);
 
   useEffect(() => {
     getUserPosition();
@@ -29,11 +31,23 @@ const Map = ({ classes }) => {
       });
     }
   };
+  const handleMapClick = ({ lngLat, leftButton }) => {
+    if (!leftButton) return;
+    if (!state.draft) {
+      dispatch({ type: "CREATE_DRAFT" });
+    }
+    const [longitude, latitude] = lngLat;
+    dispatch({
+      type: "UPDATE_DRAFT_LOCATION",
+      payload: { longitude, latitude },
+    });
+  };
 
   return (
     <div className={classes.root}>
       <ReactMapGL
         {...viewport}
+        onClick={handleMapClick}
         onViewportChange={(newViewport) => setViewport(newViewport)}
         width="100vw"
         height="calc(100vh - 64px)"
@@ -54,6 +68,17 @@ const Map = ({ classes }) => {
             offetTop={-37}
           >
             <PinIcon size={30} color="red" />
+          </Marker>
+        )}
+        {/* Draft pin */}
+        {state.draft && (
+          <Marker
+            latitude={state.draft.latitude}
+            longitude={state.draft.longitude}
+            offsetLeft={-19}
+            offetTop={-37}
+          >
+            <PinIcon size={30} color="hotpink" />
           </Marker>
         )}
       </ReactMapGL>
