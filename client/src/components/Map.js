a089e67bd8e8b3622d3263e510a9680a3baf07a7
Phonekham <30440409+Phonekham@users.noreply.github.com>
2020-12-08 11:18:01 +0700

    Creating and Styling our Map

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,42 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 import { withStyles } from "@material-ui/core/styles";
+import ReactMapGL, { NavigationControl } from "react-map-gl";
 // import Button from "@material-ui/core/Button";
 // import Typography from "@material-ui/core/Typography";
 // import DeleteIcon from "@material-ui/icons/DeleteTwoTone";
 
+const INITIAL_VIEWPORT = {
+  latitude: 17.9994624,
+  longitude: 102.6326528,
+  zoom: 13,
+};
+
 const Map = ({ classes }) => {
-  return <div>Map</div>;
+  const [viewport, setViewport] = useState(INITIAL_VIEWPORT);
+
+  return (
+    <div className={classes.root}>
+      <ReactMapGL
+        {...viewport}
+        onViewportChange={(newViewport) => setViewport(newViewport)}
+        width="100vw"
+        height="calc(100vh - 64px)"
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+        mapboxApiAccessToken="pk.eyJ1IjoicGhvbmVraGFtIiwiYSI6ImNqeGlwZDNsdTBxamIzeG10OGg0YnRtZ3oifQ.6IMNjVL6bLc-AGGbNse2VA"
+      >
+        <div className={classes.navigationControl}>
+          <NavigationControl
+            onViewportChange={(newViewport) => setViewport(newViewport)}
+          />
+        </div>
+      </ReactMapGL>
+    </div>
+  );
 };
 
 const styles = {
   root: {
-    display: "flex"
+    display: "flex",
   },
   rootMobile: {
     display: "flex",
-    flexDirection: "column-reverse"
+    flexDirection: "column-reverse",
   },
   navigationControl: {
     position: "absolute",
     top: 0,
     left: 0,
-    margin: "1em"
+    margin: "1em",
   },
   deleteIcon: {
-    color: "red"
+    color: "red",
   },
   popupImage: {
     padding: "0.4em",
     height: 200,
     width: 200,
-    objectFit: "cover"
+    objectFit: "cover",
   },
   popupTab: {
     display: "flex",
     alignItems: "center",
     justifyContent: "center",
-    flexDirection: "column"
-  }
+    flexDirection: "column",
+  },
 };
 
 export default withStyles(styles)(Map);
